38f97cdfb97484becf930e929ed98311001daa5c
jinkok <121330978+jinkok@users.noreply.github.com>
2026-01-14 20:24:39 +0900

    Update Firebase configuration with new API key

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSy...", // 여기에 본인의 API Key 입력
+    apiKey: "AIzaSyDVJRqmpFjR9g91AgQsJIV65_7BJ7yYEOU",
     authDomain: "race-app-3e41d.firebaseapp.com",
     projectId: "race-app-3e41d",
-    storageBucket: "race-app-3e41d.appspot.com",
-    messagingSenderId: "...",
-    appId: "..."
+    storageBucket: "race-app-3e41d.firebasestorage.app",
+    messagingSenderId: "232853669556",
+    appId: "1:232853669556:web:f9e7c8ccfde227f50679e1",
+    measurementId: "G-H6MB9KZ8M8"
 };
 
 // Firebase 초기화 및 내보내기
